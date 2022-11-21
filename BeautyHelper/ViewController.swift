b04ff6e0e41ce13bc3eec1c6475b7dba5ee90828
Max Hmelev <mha93@yandex.ru>
2022-11-21 20:18:40 +0300

    Initial Commit

---
+++ BeautyHelper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeautyHelper
+//
+//  Created by Максим Хмелев on 21.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
